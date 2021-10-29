1dce15fec2c57d1adf90cb1d84d788f64015c0f4
Peibin Zhang <ZhangPeibin@users.noreply.github.com>
2021-10-30 04:14:04 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,34 @@
+const {i18n} = require('./next-i18next.config');
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(['eth-hooks']);
+
+module.exports = withPlugins([withTM], {
+  i18n,
+  reactStrictMode: true,
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
+});
+// module.exports = withPWA(
+//   {
+//     i18n,
+//     reactStrictMode: true,
+//     pwa: {
+//       dest: 'public',
+//       runtimeCaching,
+//     },
+//     webpack: config => {
+//       config.module.rules.push({
+//         test: /\.svg$/,
+//         use: 'raw-loader',
+//       });
+//       return config;
+//     },
+//   }
+// );
